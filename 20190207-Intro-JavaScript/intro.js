78a7783546f00dd3cae6d163b8b9d9e0883ea884
Andrea Dottor <andrea@dottor.net>
2019-02-07 12:29:43 +0100

    aggiunto esercizio calc.js

--- 20190207-Intro-JavaScript/intro.js
+++ 20190207-Intro-JavaScript/intro.js
@@ -58,3 +58,62 @@ if(obj1)
     console.log("true");
 else 
     console.log("false");
+
+
+// ===== funzioni ====
+function add(num1, num2){
+    // arguments permette di accedere a tutti gli argomenti 
+    // della funzione, anche a quelli non dichiarati
+    console.log(arguments);
+    if(num2)
+        return num1 + num2;
+    
+    return num1;
+}
+
+var reaultAdd = add(3, 5);
+add(3, 5, 2); // il terzo valore viene passato ma non utilizzato
+var result3 = add(3);       // num2 = undefined
+console.log(result3);
+
+
+var log = function(message){
+    console.log(message);
+}
+
+log("ciao");
+
+function execAdd(num1, num2, action){
+    action(num1 + num2);
+}
+
+execAdd(2, 12, log);
+execAdd(2, 12, function(result){
+                    console.log(result);
+                });
+
+
+// ====== oggetti ====
+var user1 = new Object();
+user1.name = "Andrea";
+user1.surname = "Dottor";
+user1.getFullName = function(){
+    return this.name + " " + this.surname;
+};
+
+var user2 = {
+    name: "Andrea",
+    surname: "Dottor"
+};
+
+function User(name, surname){
+    this.name = name;
+    this.surname = surname;
+    this.getFullName = function(){
+        return this.name + " " + this.surname;
+    }
+}
+var user3 = new User("Andrea", "Dottor");
+var user4 = new User("Luca", "Dottor");
+console.log(user3.getFullName());
+user3.city = "San Vendemiano";
--- 20190207-Intro-JavaScript/readme.md
+++ 20190207-Intro-JavaScript/readme.md
@@ -3,5 +3,17 @@
 - Introduzione a JavaScript
 - Variabili
 - Tipi
-- Operatori logici
+- Operatori
+- Funzioni
+- Oggetti
 - Costrutti
+
+### Esercizio Calc.js
+
+Realizzare una funzione che crei un oggetto per eseguire un calcolo matematico.
+
+Implementare il codice di Calc in modo che il seguente blocco funzioni:
+```javascript
+var op = new Calc(123, 45, "+");
+console.log(op.exec()); // 168
+```
