26aaba80fd89223f1424c20a7e4d4dca4590e87a
Andrea Dottor <andrea@dottor.net>
2019-04-11 10:02:05 +0200

    aggiunta controllo

--- 20190411-JQuery/tictactoe/tris.js
+++ 20190411-JQuery/tictactoe/tris.js
@@ -9,6 +9,12 @@ var currentPlayer = player1Class;
 $(".pnl").on("click", function () {
     var obj = $(this);
 
+    // verifico di non aver cliccato su una cella 
+    // già selezionata in precedenza
+    if(obj.hasClass(player1Class) || obj.hasClass(player2Class)){
+        return;
+    }
+
     obj.addClass(currentPlayer)
 
     // verifico se ho vinto
@@ -22,30 +28,30 @@ $(".pnl").on("click", function () {
 });
 
 function checkWin() {
-    if (checkConbo(currentPlayer, "a1", "b1", "c1"))
+    if (checkConbo("a1", "b1", "c1"))
         return true;
-    if (checkConbo(currentPlayer, "a2", "b2", "c2"))
+    if (checkConbo("a2", "b2", "c2"))
         return true;
-    if (checkConbo(currentPlayer, "a3", "b3", "c3"))
+    if (checkConbo("a3", "b3", "c3"))
         return true;
-    if (checkConbo(currentPlayer, "a1", "a2", "a3"))
+    if (checkConbo("a1", "a2", "a3"))
         return true;
-    if (checkConbo(currentPlayer, "b1", "b2", "b3"))
+    if (checkConbo("b1", "b2", "b3"))
         return true;
-    if (checkConbo(currentPlayer, "c1", "c2", "c3"))
+    if (checkConbo("c1", "c2", "c3"))
         return true;
-    if (checkConbo(currentPlayer, "a1", "b2", "c3"))
+    if (checkConbo("a1", "b2", "c3"))
         return true;
-    if (checkConbo(currentPlayer, "a3", "b2", "c1"))
+    if (checkConbo("a3", "b2", "c1"))
         return true;
 
     return false;
 }
 
-function checkConbo(cssName, cell1, cell2, cell3) {
-    return $("#" + cell1).hasClass(cssName) &&
-            $("#" + cell2).hasClass(cssName) &&
-            $("#" + cell3).hasClass(cssName);
+function checkConbo(cell1, cell2, cell3) {
+    return $("#" + cell1).hasClass(currentPlayer) &&
+            $("#" + cell2).hasClass(currentPlayer) &&
+            $("#" + cell3).hasClass(currentPlayer);
 }
 
 function changePlayer() {
