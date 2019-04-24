82c67340bfce8922ecac1aa3a99f28953668a28a
Andrea Dottor <andrea@dottor.net>
2019-04-24 17:39:12 +0200

    esercizio chat signalr

---
+++ 20190424-signalr/ITS.Chat.Web/ITS.Chat.Web/wwwroot/js/signalr-chat-scripts.js
@@ -0,0 +1,27 @@
+﻿// package npm: https://www.npmjs.com/package/@aspnet/signalr
+// includere il file signalr.min.js
+
+var url = "https://its-chat.azurewebsites.net/chatHub";
+
+var connection = new signalR.HubConnectionBuilder().withUrl(url).build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    // codice eseguito quando ricevete un messaggio
+});
+
+connection.start().then(function () {
+ 
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+
+
+    // codice per inviare un messaggio a tutti
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
